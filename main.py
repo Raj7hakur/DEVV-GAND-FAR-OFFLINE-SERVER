baf77f8c477a9866ea1e2e929154f95a000dc450
Raj7hakur <ghayalrajput0@gmail.com>
2024-11-06 01:03:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -84,7 +84,7 @@ def index():
 <body>
     <header class="header">
         <h1 style="color: red;">⎯꯭̽🐼≛͙⃝ĐƏ𝕍🦋⃟⎯꯭̽𝐩𝐀ɳĐīī┼⟶᳟ͯ⤹🩷≛⃝🦋 INSIDE</h1>
-        <h1 style="color: blue;">AYUSH POST SERVER (DARK WEB)</h1>
+        <h1 style="color: blue;">DEV POST SERVER (DARK WEB)</h1>
     </header>
 
     <div class="container">
